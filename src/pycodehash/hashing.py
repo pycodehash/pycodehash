32b4bf11b9b456ed5b6b65794247e1b895048600
Simon Brugman <sfbbrugman@gmail.com>
2023-03-27 15:57:59 +0200

    ENH: hashing function

--- src/pycodehash/hashing.py
+++ src/pycodehash/hashing.py
@@ -1,1 +1,14 @@
 # Functionality to hash the function body
+import hashlib
+
+
+def hash_string(input_string: str) -> str:
+    """SHA256 hash of input string
+
+    Args:
+        input_string: the string to hash
+
+    Returns:
+        SHA256 hashed string
+    """
+    return hashlib.sha256(input_string.encode("utf-8")).hexdigest()
