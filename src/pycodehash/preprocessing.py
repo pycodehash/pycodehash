ae0ffa1f6db8f16198f1d09ffb3dde69fb557e0c
Ralph Urlus <rurlus.dev@gmail.com>
2023-03-30 17:13:23 +0200

    ENH: Add NodeTransformers to remove docstrings and type annotations

--- src/pycodehash/preprocessing.py
+++ src/pycodehash/preprocessing.py
@@ -1,1 +1,38 @@
 # functionality around pre-processing of the node ast
+from __future__ import annotations
+import ast
+
+from ast import NodeTransformer
+
+
+class DocstringStripper(NodeTransformer):
+    """Removes docstring node from function and class definitions."""
+
+    def visit_FunctionDef(self, node: ast.FunctionDef):
+        child = node.body[0]
+        if (
+            isinstance(child, ast.Expr)
+            and isinstance(child.value, ast.Constant)
+            and isinstance(child.value.value, str)
+        ):
+            node.body.pop(0)
+        super().generic_visit(node)
+        return node
+
+
+class TypeHintStripper(NodeTransformer):
+    """Removes type hint in the function signature and function body."""
+
+    def visit_FunctionDef(self, node):
+        node.returns = None
+        super().generic_visit(node)
+        return node
+
+    def visit_arg(self, node):
+        node.annotation = None
+        return node
+
+    def visit_AnnAssign(self, node):
+        if node.value is None:
+            return None
+        return ast.Assign([node.target], node.value, lineno=node.lineno)
