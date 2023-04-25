9ad6ed481319849f6cee29bdca4ad8e52390db97
Simon Brugman <sfbbrugman@gmail.com>
2023-04-25 11:49:26 +0200

    ENH: call visitor for inlining

---
+++ tests/test_inlining_call_visitor.py
@@ -0,0 +1,43 @@
+import ast
+
+from pycodehash.inlining import CallVisitor
+
+
+def test_call_visitor():
+    src = """
+def hello():
+    world()
+    def foo(a, b):
+        return hello(a + b + 3)
+    
+    MyClass.theMethod(3)
+    foo(1, 3)
+    foo(bar())
+    
+@decorated
+def world(c, d: float) -> str:
+    print(f"{c} {d}")
+    
+@MyClass.my_decorator
+@MyClass.another_decorator()
+@also_decorated(example="arguments")
+def bar():
+    fizzbuzz()
+    """
+
+    visitor = CallVisitor()
+    visitor.visit(ast.parse(src))
+
+    assert len(visitor.calls) == 12
+    assert visitor.calls[0] == ("call", "world")
+    assert visitor.calls[1] == ("call", "hello")
+    assert visitor.calls[2] == ("call", "MyClass.theMethod")
+    assert visitor.calls[3] == ("call", "foo")
+    assert visitor.calls[4] == ("call", "foo")
+    assert visitor.calls[5] == ("call", "bar")
+    assert visitor.calls[6] == ("decorator", "decorated")
+    assert visitor.calls[7] == ("call", "print")
+    assert visitor.calls[8] == ("decorator", "MyClass.my_decorator")
+    assert visitor.calls[9] == ("decorator call", "MyClass.another_decorator")
+    assert visitor.calls[10] == ("decorator call", "also_decorated")
+    assert visitor.calls[11] == ("call", "fizzbuzz")
