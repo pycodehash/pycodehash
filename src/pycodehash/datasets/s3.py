ebd1e00cfcdd91414cccdea260ba4bf226af28b7
Simon Brugman <sfbbrugman@gmail.com>
2023-04-25 12:17:02 +0200

    PKG: refactor local/s3 datasets to use approximate hasher

--- src/pycodehash/datasets/s3.py
+++ src/pycodehash/datasets/s3.py
@@ -1,5 +1,11 @@
+from __future__ import annotations
+
 import os
+from typing import Any
+
 import boto3
+
+from pycodehash.datasets.approximate_hasher import ApproximateHasher
 
 
 def s3path_to_bucket_key(s3_file_path: str):
@@ -9,21 +15,33 @@ def s3path_to_bucket_key(s3_file_path: str):
     return bucket, key
 
 
-def hash_file(s3_file_path: str = None, bucket: str = None, key: str = None,
-              endpoint_url: str = os.env['AWS_S3_ENDPOINT_URL']):
-    """ Get MD5 hash string of file on s3
+class S3Hash(ApproximateHasher):
+    def collect_metadata(
+        self,
+        s3_file_path: str | None = None,
+        bucket: str | None = None,
+        key: str | None = None,
+        endpoint_url: str = os.env['AWS_S3_ENDPOINT_URL']
+    ) -> dict[str, Any]:
+        """Get metadata of s3 file (specifically the MD5 hash of the file on s3)
 
-    :param str: s3 file path
-    :return: MD5 hash
-    """
-    if s3_file_path is not None:
-        bucket, key = s3path_to_bucket_key(s3_file_path=s3_file_path)
-    assert None not in [bucket, key]
+        Args:
+            s3_file_path:  path to the file on s3
+            bucket: name of the bucket
+            key: bucket key
+            endpoint_url: endpoint URL
 
-    s3 = boto3.client('s3', endpoint_url=endpoint_url)
+        Returns:
+            metadata dictionary
+        """
+        if s3_file_path is not None:
+            bucket, key = s3path_to_bucket_key(s3_file_path=s3_file_path)
+        assert None not in [bucket, key]
 
-    # The head_object() method in s3 client object will fetch the metadata (headers) of a given object
-    # stored in the s3 bucket. Not the object itself.
-    obj = s3.head_object(Bucket=bucket, Key=key)
-    etag = obj['ETag'].strip('"')
-    return etag
+        s3 = boto3.client('s3', endpoint_url=endpoint_url)
+
+        # The head_object() method in s3 client object will fetch the metadata (headers) of a given object
+        # stored in the s3 bucket. Not the object itself.
+        obj = s3.head_object(Bucket=bucket, Key=key)
+        etag = obj['ETag'].strip('"')
+        return {"etag": etag}
