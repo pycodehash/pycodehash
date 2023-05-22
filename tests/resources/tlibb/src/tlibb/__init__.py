7dd3f7638e8b842fa0ac7e29269453d7ee1199a2
Simon Brugman <sfbbrugman@gmail.com>
2023-05-22 14:19:25 +0200

    TST: simplify inline testng

---
+++ tests/resources/tlibb/src/tlibb/__init__.py
@@ -0,0 +1,4 @@
+from tliba.summary import compute_moments
+from tliba.summary import compute_conditional_moments
+
+__all__ = ["compute_moments", "compute_conditional_moments"]
---
+++ tests/resources/tlibb/src/tlibb/etl.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+import tliba.random.rng as rng
+from tliba.random import draw_bernoulli_samples
+from tliba.random import draw_normal_samples as normal_samples
+
+
+def combine_random_samples():
+    print("Concat")
+    return pd.concat((rng.draw_beta_samples(), normal_samples()), axis=1)
+
+
+def add_bernoulli_samples():
+    print("Hello world")
+    df = combine_random_samples().copy()
+    df["bernoulli"] = draw_bernoulli_samples()
+    return df
