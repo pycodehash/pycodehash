8647cad88e8c97fc81f397b70da0e4b3dbeca86e
Simon Brugman <sfbbrugman@gmail.com>
2023-05-28 13:04:32 +0200

    test: decorator stripper

--- src/pycodehash/preprocessing/decorator_stripper.py
+++ src/pycodehash/preprocessing/decorator_stripper.py
@@ -3,21 +3,19 @@ from __future__ import annotations
 import ast
 from ast import NodeTransformer
 
-from pycodehash.unparse import _unparse
 
-
-def get_decorator_name(decorator: ast.expr):
+def _get_decorator_name(decorator: ast.expr):
     if isinstance(decorator, ast.Name):
         return decorator.id
     if isinstance(decorator, ast.Call):
-        return get_decorator_name(decorator.func)
+        return _get_decorator_name(decorator.func)
     if isinstance(decorator, ast.Attribute):
-        return get_decorator_name(decorator.value) + "." + decorator.attr
+        return _get_decorator_name(decorator.value) + "." + decorator.attr
 
     raise NotImplementedError
 
 
-class FunctionStripper(NodeTransformer):
+class DecoratorStripper(NodeTransformer):
     """Removes specific decorators from functions."""
     def __init__(self, decorators: list[str]):
         self.decorators = decorators
@@ -26,28 +24,6 @@ class FunctionStripper(NodeTransformer):
         node.decorator_list = [
             decorator
             for decorator in node.decorator_list
-            if get_decorator_name(decorator) not in self.decorators
+            if _get_decorator_name(decorator) not in self.decorators
         ]
         return node
-
-
-if __name__ == "__main__":
-    src = """import functools
-from functools import lru_cache
-
-def hello(x):
-    return x
-    
-@hello()
-@hello
-@functools.lru_cache()
-@functools.lru_cache
-@lru_cache()
-@lru_cache
-def world():
-    pass
-"""
-    src = ast.parse(src)
-    a = FunctionStripper(decorators=["functools.lru_cache"])
-    x = a.visit(src)
-    print(_unparse(x))
