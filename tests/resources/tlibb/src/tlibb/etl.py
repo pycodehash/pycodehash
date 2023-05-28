106c8ccc5dea7a7f7f7e8d66b42efff43cfe1248
Simon Brugman <sfbbrugman@gmail.com>
2023-05-28 12:51:57 +0200

    add edge case testing

--- tests/resources/tlibb/src/tlibb/etl.py
+++ tests/resources/tlibb/src/tlibb/etl.py
@@ -3,6 +3,7 @@ import pandas as pd
 import tliba.random.rng as rng
 from tliba.random import draw_bernoulli_samples
 from tliba.random import draw_normal_samples as normal_samples
+from tlibb.constant import nanmax
 
 
 def combine_random_samples():
@@ -14,4 +15,5 @@ def add_bernoulli_samples():
     print("Hello world")
     df = combine_random_samples().copy()
     df["bernoulli"] = draw_bernoulli_samples()
+    nanmax(4)
     return df
