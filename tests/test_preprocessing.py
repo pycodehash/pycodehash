5968be1e8c1710865311446257256f764aa8551e
Simon Brugman <sfbbrugman@gmail.com>
2023-05-28 13:04:32 +0200

    test: decorator stripper

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,6 +1,7 @@
 import ast
 
 from pycodehash.preprocessing import DocstringStripper, TypeHintStripper, WhitespaceNormalizer, FunctionStripper
+from pycodehash.preprocessing.decorator_stripper import DecoratorStripper
 from pycodehash.unparse import _unparse
 
 
@@ -208,5 +209,38 @@ def test_function_name_stripper():
         "    y = y or 10\n"
         "    z = 2 * x\n"
         "    return z + y"
+    )
+    assert processed == f_str_ref
+
+
+def test_decorator_stripper():
+    f_str = (
+        "import functools\n"
+        "from functools import lru_cache\n"
+        "\n"
+        "def hello(x):\n"
+        "    return x\n"
+        "\n"
+        "@hello()\n"
+        "@hello\n"
+        "@functools.lru_cache()\n"
+        "@functools.lru_cache\n"
+        "@lru_cache()\n"
+        "@lru_cache\n"
+        "def world():\n"
+        "    pass\n"
+    )
+    processed = _unparse(DecoratorStripper(decorators=["functools.lru_cache"]).visit(ast.parse(f_str)))
+    f_str_ref = (
+        "import functools\n"
+        "from functools import lru_cache\n"
+        "\n"
+        "def hello(x):\n"
+        "    return x\n"
+        "\n"
+        "@hello()\n"
+        "@hello\n"
+        "def world():\n"
+        "    pass\n"
     )
     assert processed == f_str_ref
