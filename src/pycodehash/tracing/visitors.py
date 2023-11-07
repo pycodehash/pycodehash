1e98f593b1a45128286dd117739b201606e79013
Ralph Urlus <rurlus.dev@gmail.com>
2023-11-07 10:41:04 +0100

    specialise the defintion finding functions

--- src/pycodehash/tracing/visitors.py
+++ src/pycodehash/tracing/visitors.py
@@ -3,33 +3,13 @@
 from __future__ import annotations
 
 import ast
-from ast import NodeTransformer, NodeVisitor
+from ast import NodeVisitor
 
 from rope.base.libutils import path_to_resource
-from rope.contrib.findit import Location, find_definition
+from rope.contrib.findit import Location
 
-from pycodehash.hashing import hash_string
 from pycodehash.tracing.stores import ModuleView, ProjectStore
-
-
-class HashCalls(NodeTransformer):
-    def find_definition(self, node, project) -> Location | None:
-        loc = find_definition(project, self.module.code, self.module.tree_tokens.get_text_range(node)[0])
-        if isinstance(loc, Location) and loc.resource is None:
-            loc.resource = path_to_resource(project, self.module.path)
-        return loc
-
-    def visit_Call(self, node: ast.Call):
-        # call a function that hashes the source of the function
-        self.hash_repr = hash_string(ast.unparse(node))
-        super().generic_visit(node)
-        return node
-
-    def visit_Name(self, node: ast.Name):
-        if self.hash_repr is not None:
-            node.id = self.hash_repr
-            self.hash_repr = None
-        return node
+from pycodehash.tracing.utils import get_func_def_location
 
 
 class CallDefinitionTracer(NodeVisitor):
@@ -45,7 +25,7 @@ class CallDefinitionTracer(NodeVisitor):
         self.projects = projects
 
     def find_definition(self, node, project) -> Location | None:
-        loc = find_definition(project, self.module.code, self.module.tree_tokens.get_text_range(node)[0])
+        loc = get_func_def_location(node, project, self.module)
         if isinstance(loc, Location) and loc.resource is None:
             loc.resource = path_to_resource(project, self.module.path)
         return loc
