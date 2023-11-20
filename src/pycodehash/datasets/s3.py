1a26343dc0b6477598ae6029d19d588403b8d62a
mbaak <maxbaak@gmail.com>
2023-11-20 21:18:36 +0100

    fix review comments

--- src/pycodehash/datasets/s3.py
+++ src/pycodehash/datasets/s3.py
@@ -18,13 +18,29 @@ def s3path_to_bucket_key(s3_file_path: str):
 
 
 class S3Hash(ApproximateHasher):
+    def __init__(
+        self,
+        s3_client: Any = None,
+        endpoint_url: str = os.env["AWS_S3_ENDPOINT_URL"],
+    ):
+        """Initialization of S3Hash
+
+        Args:
+            s3_client: boto3 s3 client
+            endpoint_url: endpoint URL in case s3 client is not provided
+        """
+        super().__init__()
+
+        if s3_client is None:
+            s3_client = boto3.client("s3", endpoint_url=endpoint_url)
+
+        self.s3_client = s3_client
+
     def collect_metadata(
         self,
         s3_file_path: str | None = None,
         bucket: str | None = None,
         key: str | None = None,
-        s3_client: Any = None,
-        endpoint_url: str = os.env["AWS_S3_ENDPOINT_URL"],
     ) -> dict[str, Any]:
         """Get metadata of s3 file (specifically the MD5 hash of the file on s3)
 
@@ -32,8 +48,6 @@ class S3Hash(ApproximateHasher):
             s3_file_path:  path to the file on s3
             bucket: name of the bucket
             key: bucket key
-            s3_client: boto3 s3 client
-            endpoint_url: endpoint URL in case s3 client is not provided
 
         Returns:
             metadata dictionary
@@ -42,21 +56,16 @@ class S3Hash(ApproximateHasher):
             bucket, key = s3path_to_bucket_key(s3_file_path=s3_file_path)
         assert None not in [bucket, key]
 
-        if s3_client is None:
-            s3_client = boto3.client("s3", endpoint_url=endpoint_url)
-
         # The list_objects_v2() method in s3 client object will fetch the metadata (headers) of a given object
         # stored in the s3 bucket. (Not the object itself.) If key is a directory it will retrieve all object in the
         # directory in alphabetic order. Per object: Etag (md5 hash), size, last modification data, and storage class.
-        files = s3_client.list_objects_v2(Bucket=bucket, Prefix=key)
+        files = self.s3_client.list_objects_v2(Bucket=bucket, Prefix=key)
         for file in files:
             # clean up the ETag hash, remove unnecessary quotes.
             file["ETag"] = file["ETag"].strip('"')
 
-        dict_to_hash = {}
-        for idx, file in enumerate(files):
-            keys = list(file.keys())
-            for key in keys:
-                dict_to_hash[f"__file{idx}__{key}"] = file[key]
-
-        return dict_to_hash
+        return {
+            f"__file{idx}__{key}": value
+            for idx, file in enumerate(files)
+            for key, value in file.items()
+        }
