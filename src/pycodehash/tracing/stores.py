0bd577a582ad3e694d4ff071f0dd8aeb64a87df4
Simon Brugman <sfbbrugman@gmail.com>
2023-11-23 15:09:43 +0100

    simplify stores

--- src/pycodehash/tracing/stores.py
+++ src/pycodehash/tracing/stores.py
@@ -1,12 +1,9 @@
 from __future__ import annotations
 
 import ast
-import inspect
 from dataclasses import dataclass
-from functools import singledispatchmethod
 from importlib.util import find_spec
 from pathlib import Path
-from types import FunctionType, ModuleType
 
 import asttokens
 from rope.base.libutils import analyze_modules
@@ -16,18 +13,13 @@ from rope.contrib.findit import Location
 
 
 class FunctionStore:
-    # TODO(SB): perhaps just a typed dict instead of classes?
     def __init__(self):
         self.store: dict[Location, str] = {}
-
-    def get(self, location: Location) -> str:
-        """Get hash of function at `location`."""
-        ...
 
     def __getitem__(self, item: Location) -> str:
         return self.store[item]
 
-    def __setitem__(self, key, value):
+    def __setitem__(self, key: Location, value: str) -> None:
         self.store[key] = value
 
 
@@ -45,135 +37,56 @@ class ModuleStore:
     def __init__(self):
         self.store: dict[str, ModuleView] = {}
 
-    @singledispatchmethod
-    def _get_module_name(self, mod) -> str:
-        raise NotImplementedError("`mod` must a `ModuleType` or `rope.PyModule`.")
-
-    @_get_module_name.register
-    def _get_module_name_module_type(self, mod: ModuleType) -> str:
-        return mod.__name__
-
-    @_get_module_name.register
-    def _get_module_name_pymodule(self, mod: PyModule) -> str:
-        return mod.get_name()
-
-    @singledispatchmethod
-    def _create_module_view(self, mod) -> ModuleView:
-        raise NotImplementedError("`mod` must a `ModuleType` or `rope.PyModule`.")
-
-    @_create_module_view.register
-    def _create_module_view_from_module_type(self, mod: ModuleType) -> ModuleView:
-        name = mod.__name__
-        pkg, _sep, _stem = name.partition(".")
-        path = Path(inspect.getsourcefile(mod))
-        code = path.read_text()
-        tree = ast.parse(code)
-
-        return ModuleView(
-            pkg=pkg,
-            name=name,
-            path=path,
-            code=code,
-            tree=tree,
-            tree_tokens=asttokens.ASTTokens(code, parse=False, tree=tree),
-        )
-
-    @_create_module_view.register
-    def _create_module_view_from_pymodule(self, mod: PyModule) -> ModuleView:
-        name = mod.get_name()
-        pkg, _sep, _stem = name.partition(".")
-        path = mod.get_resource().pathlib
-        code = mod.resource.read()
-        tree = mod.get_ast()
-
-        return ModuleView(
-            pkg=pkg,
-            name=name,
-            path=path,
-            code=code,
-            tree=tree,
-            tree_tokens=asttokens.ASTTokens(code, parse=False, tree=tree),
-        )
-
-    def set(self, func: FunctionType, error_if_set: bool = False):
-        mod = inspect.getmodule(func)
-        name = mod.__name__
-        if name in self.store and error_if_set:
-            raise ValueError(f"Module with name {name} has been previously set.")
-        mod_view = self._create_module_view(mod)
-        self.store[name] = mod_view
-
-    @singledispatchmethod
-    def get(self, key, error_if_missing: bool = False) -> ModuleView:
-        """Retrieve ModuleView for `key`.
-
-        Args:
-            key (FunctionType | str): the function for which to retrieve its module or the name of the module
-            error_if_missing: raise error if `key` is unknown. If False and the `key` is a FunctionType than
-                a ModuleView is created and returned, if the `key` is a string that None is returned.
-
-        Raises:
-            KeyError: when `key` is unknown and `error_if_missing` is True
-            NotImplementedError: when type of `key` is not a string or FunctionType
-
-        """
-        raise NotImplementedError("`key` must a `str` or `FunctionType`.")
-
-    @get.register(ModuleType)
-    @get.register(PyModule)
-    def get_from_module(self, key: ModuleType | PyModule, error_if_missing: bool = False) -> ModuleView:
+    def __getitem__(self, item: PyModule) -> ModuleView:
         """Retrieve ModuleView from module.
 
         Args:
-            key: the module for which to retrieve its view
-            error_if_missing: raise error if `key` is unknown, otherwise a ModuleView is created
-                and returned.
+            item: the module for which to retrieve its view
 
         Raises:
-            KeyError: when `key` is unknown and `error_if_missing` is True
+            TypeError: when the key is not a Python Module object or a `rope` module
 
         """
-        name = self._get_module_name(key)
-        mod_view = self.store.get(name)
+        name = item.get_name()
+        if name not in self.store:
+            self._initialize_module_view(name, item)
+        return self.store[name]
 
-        if mod_view is not None:
-            return mod_view
-        if error_if_missing is True:
-            raise KeyError(f"Unknown module: {name}")
-        mod_view = self._create_module_view(key)
-        self.store[name] = mod_view
-        return mod_view
+    def _initialize_module_view(self, name, module: PyModule) -> None:
+        pkg, _, _ = name.partition(".")
+        path = module.get_resource().pathlib
+        code = module.resource.read()
+        tree = module.get_ast()
+        tree_tokens = asttokens.ASTTokens(code, parse=False, tree=tree)
 
-    @get.register
-    def get_from_func(self, key: FunctionType, error_if_missing: bool = False) -> ModuleView:
-        """Retrieve ModuleView from func.
-
-        Args:
-            key: the function for which to retrieve its module
-            error_if_missing: raise error if `key` is unknown, otherwise a ModuleView is created
-                and returned.
-
-        Raises:
-            KeyError: when `key` is unknown and `error_if_missing` is True
-
-        """
-        mod = inspect.getmodule(key)
-        return self.get_from_module(mod, error_if_missing)
-
-    @get.register
-    def get_from_name(self, key: str, error_if_missing: bool = False) -> ModuleView:
-        mod_view = self.store.get(key)
-        if error_if_missing is True and mod_view is None:
-            raise KeyError(f"Unknown name: {key}")
-        # TODO(SB): set mod_view = Default value
-        return mod_view
+        self.store[name] = ModuleView(
+            pkg=pkg,
+            name=name,
+            path=path,
+            code=code,
+            tree=tree,
+            tree_tokens=tree_tokens,
+        )
 
 
 class ProjectStore:
     def __init__(self):
         self.store: dict[str, Project] = {}
 
-    def set(self, pkg: str):
+    def __getitem__(self, item: str) -> Project:
+        """Get rope project from package name.
+
+        Args:
+            item: package name
+
+        Returns:
+            project: analyzed rope project
+        """
+        if item not in self.store:
+            self._initialize_project(item)
+        return self.store[item]
+
+    def _initialize_project(self, pkg: str):
         """Create and set a project.
 
         Args:
@@ -188,22 +101,10 @@ class ProjectStore:
         analyze_modules(project)
         self.store[pkg] = project
 
-    def get(self, pkg: str) -> Project:
-        """Get rope project from package name.
-
-        Args:
-            pkg: package name
-
-        Returns:
-            project: analyzed rope project
-        """
-        if pkg not in self.store:
-            self.set(pkg)
-        return self.store[pkg]
-
+    # TODO: this should be refactored
     def get_projects(self, mod: ModuleView | None = None) -> list[Project]:
         """Create a list with all projects where the first project to which the module belongs to."""
         if mod is None:
             return list(self.store.values())
-        mod_project = self.get(mod.pkg)
+        mod_project = self[mod.pkg]
         return [mod_project] + [v for v in self.store.values() if v != mod_project]
