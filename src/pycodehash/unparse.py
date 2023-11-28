ffa19a8b02fe391d8b25c88cf29fa08ae4a57b7f
Simon Brugman <sfbbrugman@gmail.com>
2023-11-28 20:30:00 +0100

    FIX: backwards compatibility

--- src/pycodehash/unparse.py
+++ src/pycodehash/unparse.py
@@ -1,7 +1,7 @@
 """Backwards compatibility unparsing for <= py3.8"""
 import sys
 
-if sys.version_info < (3, 8):
+if sys.version_info < (3, 9):
     # when python 3.9 is common, this dependency can be removed
     # as python 3.9 has the corresponding native function
     from astunparse import unparse
