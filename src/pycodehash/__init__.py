2e275358cf5b8e3d821c1ddb994d9e51aa0b3e34
Ralph Urlus <rurlus.dev@gmail.com>
2023-11-28 10:47:48 +0100

    chg: export all definitions required to configure FunctionHasher

--- src/pycodehash/__init__.py
+++ src/pycodehash/__init__.py
@@ -1,3 +1,19 @@
 from pycodehash.hashing import FunctionHasher
+from pycodehash.preprocessing import (
+    DecoratorStripper,
+    DocstringStripper,
+    FunctionStripper,
+    TypeHintStripper,
+    WhitespaceNormalizer,
+)
+from pycodehash.version import __version__
 
-__all__ = ["FunctionHasher"]
+__all__ = [
+    "DecoratorStripper",
+    "DocstringStripper",
+    "FunctionHasher",
+    "FunctionStripper",
+    "TypeHintStripper",
+    "WhitespaceNormalizer",
+    "__version__",
+]
