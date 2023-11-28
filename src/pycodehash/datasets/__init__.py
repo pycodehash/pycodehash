2cc27774c104c46803ccdf140b727d2a2c877bee
Simon Brugman <sfbbrugman@gmail.com>
2023-11-28 17:22:12 +0100

    ENH: export dataset functionality

--- src/pycodehash/datasets/__init__.py
+++ src/pycodehash/datasets/__init__.py
@@ -1,4 +1,13 @@
 """Embarrassingly fast approximate hashes for specific datasets"""
+from pycodehash.datasets.approximate_hasher import ApproximateHasher, PartitionedApproximateHasher
 from pycodehash.datasets.hive import HiveTableHash
+from pycodehash.datasets.local import LocalDirectoryHash, LocalFileHash, hash_file_full
 
-__all__ = ["HiveTableHash"]
+__all__ = [
+    "HiveTableHash",
+    "LocalFileHash",
+    "LocalDirectoryHash",
+    "ApproximateHasher",
+    "PartitionedApproximateHasher",
+    "hash_file_full",
+]
