9f5f3529565e36f446c3796529317f6ee1fe6d54
Simon Brugman <sfbbrugman@gmail.com>
2023-11-28 20:34:33 +0100

    FIX: spark optional

--- src/pycodehash/datasets/__init__.py
+++ src/pycodehash/datasets/__init__.py
@@ -1,10 +1,8 @@
 """Embarrassingly fast approximate hashes for specific datasets"""
 from pycodehash.datasets.approximate_hasher import ApproximateHasher, PartitionedApproximateHasher
-from pycodehash.datasets.hive import HiveTableHash
 from pycodehash.datasets.local import LocalDirectoryHash, LocalFileHash, hash_file_full
 
 __all__ = [
-    "HiveTableHash",
     "LocalFileHash",
     "LocalDirectoryHash",
     "ApproximateHasher",
