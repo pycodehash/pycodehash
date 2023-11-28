ddd84f174e0fe7c053eaa6ae6ed3b7176a892aa6
Ralph Urlus <rurlus.dev@gmail.com>
2023-11-28 13:11:13 +0100

    chg: remove debug statements

--- src/pycodehash/preprocessing/function_stripper.py
+++ src/pycodehash/preprocessing/function_stripper.py
@@ -1,16 +1,12 @@
 from __future__ import annotations
 
 import ast
-import logging
 from ast import NodeTransformer
-
-logger = logging.getLogger(__name__)
 
 
 class FunctionStripper(NodeTransformer):
     """Removes docstring node from function and class definitions."""
 
     def visit_FunctionDef(self, node: ast.FunctionDef):
-        logger.debug(f"Replace function name `{node.name}` with `_`")
         node.name = "_"
         return node
--- src/pycodehash/preprocessing/typehint_stripper.py
+++ src/pycodehash/preprocessing/typehint_stripper.py
@@ -1,10 +1,7 @@
 from __future__ import annotations
 
 import ast
-import logging
 from ast import NodeTransformer
-
-logger = logging.getLogger(__name__)
 
 
 class TypeHintStripper(NodeTransformer):
@@ -12,19 +9,16 @@ class TypeHintStripper(NodeTransformer):
 
     def visit_FunctionDef(self, node):
         if node.returns is not None:
-            logger.debug("Strip type hints from function")
             node.returns = None
         super().generic_visit(node)
         return node
 
     def visit_arg(self, node):
         if node.annotation is not None:
-            logger.debug("Strip type hints from argument")
             node.annotation = None
         return node
 
     def visit_AnnAssign(self, node):
         if node.value is None:
-            logger.debug("Strip type hints from assignment")
             return None
         return ast.Assign([node.target], node.value, lineno=node.lineno)
