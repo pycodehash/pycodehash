44a878c7eeed396de405a24ef0cbd5dc4561766a
Simon Brugman <sfbbrugman@gmail.com>
2023-12-03 23:32:39 +0100

    TYP: fix type hints in TypeHintStripper

--- src/pycodehash/preprocessing/typehint_stripper.py
+++ src/pycodehash/preprocessing/typehint_stripper.py
@@ -7,16 +7,16 @@ from ast import NodeTransformer
 class TypeHintStripper(NodeTransformer):
     """Removes the type hints in the function signature and function body."""
 
-    def visit_FunctionDef(self, node: ast.expr):
+    def visit_FunctionDef(self, node: ast.FunctionDef):
         if node.returns is not None:
             node.returns = None
         super().generic_visit(node)
         return node
 
-    def visit_arg(self, node: ast.expr):
+    def visit_arg(self, node: ast.arg):
         if node.annotation is not None:
             node.annotation = None
         return node
 
-    def visit_AnnAssign(self, node: ast.expr):
+    def visit_AnnAssign(self, node: ast.AnnAssign):
         return ast.Assign([node.target], node.value, lineno=node.lineno)
