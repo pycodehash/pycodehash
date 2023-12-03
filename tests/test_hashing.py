da13db4108602ab6dfc5a1d8839783f3c38e180a
Simon Brugman <sfbbrugman@gmail.com>
2023-12-03 23:33:30 +0100

    MAINT: typo

--- tests/test_hashing.py
+++ tests/test_hashing.py
@@ -19,7 +19,7 @@ _REFERNCE_HASHES: dict[FunctionType, str] = {
 
 
 def test_no_fp_calls():
-    """Test hash of refernce without first party calls."""
+    """Test hash of reference without first party calls."""
     tfunc = tliba.random.draw_beta_samples
     fh = FunctionHasher()
     result = fh.hash_func(tfunc)
@@ -28,7 +28,7 @@ def test_no_fp_calls():
 
 
 def test_within_module_fp_calls():
-    """Test hash of reference with within module first party calls."""
+    """Test hash of reference with within module first-party calls."""
     tfunc = tliba.random.draw_beta_samples
     fh = FunctionHasher()
     result = fh.hash_func(tfunc)
@@ -48,7 +48,7 @@ def test_across_module_fp_calls():
 def test_across_package_calls():
     """Test hash of reference with across pacakge first party calls."""
     tfunc = tlibb_etl_combine_random_samples
-    # we first check that we don't trace funtions if we don't explicitely set tliba
+    # we first check that we don't trace functions if we don't explicitly set tliba
     fh = FunctionHasher()
     result = fh.hash_func(tfunc)
     ref_no_tliba = "b67a8c52603f12272b25c2c3a513cd14c7cfddf8163678897ab3f0986cf9fc8b"
