0d1306debf0f91e6140e4bdba497112847c29578
Simon Brugman <sfbbrugman@gmail.com>
2023-12-04 14:02:29 +0100

    DOC: update examples with new syntax

--- example_data.py
+++ example_data.py
@@ -6,14 +6,14 @@ from pycodehash.datasets import LocalDirectoryHash, LocalFileHash
 fh = LocalFileHash()
 
 print(fh.collect_metadata("example.py"))
-# {'last_modified': datetime.datetime(2023, 11, 24, 23, 38, 17, 524024), 'size': 621}
+# {'last_modified': datetime.datetime(2023, 11, 24, 23, 38, 17, 524024), 'size': 543}
 
 print(fh.compute_hash("example.py"))
-# 2582e4198b42e6ceedeb58e05925da2480cdafddf87a731b971f315352beca47
+# 6189721d3ecdf86503a82c07eed82743069ebbf39e974f33ca684809e67e9e0e
 
 # Hash a directory
 dh = LocalDirectoryHash()
 
 # Recursively hash all files in a directory
-print(len(dh.collect_hashes(Path(__file__).parent / "src")))
+print(len(dh.collect_partitions(Path(__file__).parent / "src")))
 # 29
