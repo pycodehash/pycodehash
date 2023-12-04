b00bdbc7cb64a1ebeed0f9f50eabc21a771ff83b
Simon Brugman <sfbbrugman@gmail.com>
2023-12-04 12:30:02 +0100

    FIX: avoid duplication in LocalDirectoryHash
    Make hash invariant to changes in absolute path

--- tests/datasets/test_local.py
+++ tests/datasets/test_local.py
@@ -18,6 +18,10 @@ def local_dataset_directory(tmp_path):
     file_name2.write_text("Hello Mars!")
     file_name3 = tmp_path / "my_file3.txt"
     file_name3.write_text("Hello Venus!")
+    dir_name1 = tmp_path / "subdir"
+    dir_name1.mkdir(exist_ok=True)
+    file_name4 = dir_name1 / "my_file4.txt"
+    file_name4.write_text("Hello Saturn!")
     return tmp_path
 
 
@@ -49,11 +53,12 @@ def test_approximate_hasher_local_file(local_dataset):
 def test_approximate_hasher_local_directory(local_dataset_directory):
     hasher = LocalDirectoryHash()
     partitions = hasher.collect_partitions(local_dataset_directory)
-    assert len(partitions) == 3
+    assert len(partitions) == 4
+    assert set(partitions.keys()) == {"my_file2.txt", "my_file3.txt", "my_file1.txt", "subdir/my_file4.txt"}
 
     initial_metadata = hasher.collect_metadata(local_dataset_directory)
     assert isinstance(initial_metadata, dict)
-    assert len(initial_metadata.keys()) == 3
+    assert len(initial_metadata.keys()) == 4
 
     initial_hash = hasher.compute_hash(local_dataset_directory)
     assert isinstance(initial_hash, str)
@@ -61,11 +66,11 @@ def test_approximate_hasher_local_directory(local_dataset_directory):
     second_hash = hasher.compute_hash(local_dataset_directory)
     assert initial_hash == second_hash
 
-    file_name4 = local_dataset_directory / "my_file4.txt"
-    file_name4.write_text("Hello World!")
+    file_name5 = local_dataset_directory / "my_file5.txt"
+    file_name5.write_text("Hello Jupiter!")
 
     third_hash = hasher.compute_hash(local_dataset_directory)
     assert initial_hash != third_hash
 
     third_metadata = hasher.collect_metadata(local_dataset_directory)
-    assert len(third_metadata.keys()) == 4
+    assert len(third_metadata.keys()) == 5
