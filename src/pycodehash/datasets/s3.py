aea5fc436ab04472595f17d03221c550cbc06320
mbaak <maxbaak@gmail.com>
2023-12-08 14:49:47 +0100

    ENH: added possibility to hash a directory of files on s3

--- src/pycodehash/datasets/s3.py
+++ src/pycodehash/datasets/s3.py
@@ -39,6 +39,7 @@ class S3Hash(ApproximateHasher):
         self, s3_file_path: str | None = None, bucket: str | None = None, key: str | None = None
     ) -> dict[str, Any]:
         """Get metadata of s3 file (specifically the MD5 hash of the file on s3)
+        [Documentation of `list_objects_v2`](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3/client/list_objects_v2.html)
 
         Args:
             s3_file_path: path to the file on s3
@@ -47,13 +48,20 @@ class S3Hash(ApproximateHasher):
 
         Returns:
             metadata dictionary
+
         """
         if s3_file_path is not None:
             bucket, key = s3path_to_bucket_key(s3_file_path=s3_file_path)
         assert None not in {bucket, key}
 
-        # The head_object() method in s3 client object will fetch the metadata (headers) of a given object
-        # stored in the s3 bucket. Not the object itself.
-        obj = self.s3_client.head_object(Bucket=bucket, Key=key)
-        etag = obj["ETag"].strip('"')
-        return {"etag": etag}
+        # The list_objects_v2() method in s3 client object will fetch the metadata (headers) of a given object
+        # stored in the s3 bucket. (Not the object itself.) If key is a directory, it will retrieve all objects in the
+        # directory in alphabetic order. Per object: ETag (md5 hash), size, last modification data, and storage class.
+        response = self.s3_client.list_objects_v2(Bucket=bucket, Prefix=key)
+        files = response["Contents"]
+
+        for file in files:
+            # clean up the ETag hash, remove unnecessary quotes.
+            file["ETag"] = file["ETag"].strip('"')
+
+        return {f"__file{idx}__ETag": file["ETag"] for idx, file in enumerate(sorted(files, key=lambda x: x["Key"]))}
