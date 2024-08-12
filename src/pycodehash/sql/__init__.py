6bcfc4e992fdb186c7af3af38e5af75324fce9d7
Simon Brugman <sfbbrugman@gmail.com>
2024-08-12 15:07:04 +0200

    ENH: switch to SQLAnalyze

--- src/pycodehash/sql/__init__.py
+++ src/pycodehash/sql/__init__.py
@@ -1,11 +1,5 @@
-from pycodehash.sql.references import EnrichedTableReferenceVisitor, TableReferenceVisitor, extract_table_references
+from pycodehash.sql.references import extract_table_references
 from pycodehash.sql.sql_hasher import SQLHasher
 from pycodehash.sql.whitespace_filter import WhitespaceFilter
 
-__all__ = [
-    "TableReferenceVisitor",
-    "EnrichedTableReferenceVisitor",
-    "extract_table_references",
-    "SQLHasher",
-    "WhitespaceFilter",
-]
+__all__ = ["extract_table_references", "SQLHasher", "WhitespaceFilter"]
--- tests/sql/test_references.py
+++ tests/sql/test_references.py
@@ -1,95 +1,4 @@
-from pycodehash.sql import WhitespaceFilter
-from pycodehash.sql.references import (
-    EnrichedTableReferenceVisitor,
-    TableReferenceVisitor,
-    _table_reference_to_string,
-    extract_table_references,
-)
-from sqlfluff import parse
-
-
-def test_table_reference_to_string():
-    assert _table_reference_to_string({"naked_identifier": "my_table"}) == "my_table"
-    assert (
-        _table_reference_to_string([{"naked_identifier": "src"}, {"dot": "."}, {"naked_identifier": "my_table"}])
-        == "src.my_table"
-    )
-
-
-def test_find_table_references():
-    query = {
-        "file": [
-            {
-                "statement": {
-                    "create_table_statement": [
-                        {"keyword": "CREATE"},
-                        {"whitespace": " "},
-                        {"keyword": "TABLE"},
-                        {"whitespace": " "},
-                        {"table_reference": {"naked_identifier": "db.my_table"}},
-                        {"whitespace": " "},
-                        {"keyword": "AS"},
-                        {"whitespace": " "},
-                        {
-                            "select_statement": {
-                                "select_clause": {
-                                    "keyword": "SELECT",
-                                    "whitespace": " ",
-                                    "select_clause_element": {
-                                        "wildcard_expression": {"wildcard_identifier": {"star": "*"}}
-                                    },
-                                },
-                                "whitespace": " ",
-                                "from_clause": {
-                                    "keyword": "FROM",
-                                    "whitespace": " ",
-                                    "from_expression": {
-                                        "from_expression_element": {
-                                            "table_expression": {
-                                                "table_reference": {"naked_identifier": "db.that_table"}
-                                            }
-                                        }
-                                    },
-                                },
-                            }
-                        },
-                    ]
-                }
-            },
-            {"statement_terminator": ";"},
-            {"whitespace": " "},
-            {
-                "statement": {
-                    "select_statement": {
-                        "select_clause": {
-                            "keyword": "SELECT",
-                            "whitespace": " ",
-                            "select_clause_element": {"wildcard_expression": {"wildcard_identifier": {"star": "*"}}},
-                        },
-                        "whitespace": " ",
-                        "from_clause": {
-                            "keyword": "FROM",
-                            "whitespace": " ",
-                            "from_expression": {
-                                "from_expression_element": {
-                                    "table_expression": {"table_reference": {"naked_identifier": "db.my_table"}}
-                                }
-                            },
-                        },
-                    }
-                }
-            },
-        ]
-    }
-
-    v = TableReferenceVisitor()
-    v.generic_visit(query)
-    assert v.references == ["db.my_table", "db.that_table", "db.my_table"]
-    assert extract_table_references(query) == {"db.my_table", "db.that_table"}
-
-    ev = EnrichedTableReferenceVisitor()
-    ev.generic_visit(query)
-    assert ev.references == [("CREATE", "db.my_table"), ("FROM", "db.that_table"), ("FROM", "db.my_table")]
+from pycodehash.sql.references import extract_table_references
 
 
 def test_enriched_table_reference_visitor_cte():
@@ -111,22 +20,10 @@ def test_enriched_table_reference_visitor_cte():
             intermediate_table
     )
     """
-    ast = parse(query, dialect="sparksql")
-    ev = EnrichedTableReferenceVisitor()
-    ev.generic_visit(ast)
-    assert ev.references == [
-        ("CREATE", "output_table"),
-        ("CTE", "intermediate_table"),
-        ("FROM", "sdb.source_table1"),
-        ("FROM", "sdb.source_table2"),
-        ("FROM", "sdb.source_table3"),
-        ("CTE", "another_intermediate_table"),
-        ("FROM", "hello_world"),
-        ("FROM", "intermediate_table"),
-    ]
+    input_tables, output_tables, dropped_tables = extract_table_references(
+        query, dialect="sparksql", default_db="hello_world"
+    )
 
-    # Without whitespace should yield identical results
-    ast = WhitespaceFilter().transform(ast)
-    ewv = EnrichedTableReferenceVisitor()
-    ewv.generic_visit(ast)
-    assert ewv.references == ev.references
+    assert input_tables == {"sdb.source_table3", "sdb.source_table2", "hello_world.hello_world", "sdb.source_table1"}
+    assert output_tables == {"hello_world.output_table"}
+    assert dropped_tables == set()
